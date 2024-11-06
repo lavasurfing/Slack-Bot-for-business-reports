c8754b46461907af02bc1ca673b53bdd415d9ebe
AshishOnCodes <87165421+lavasurfing@users.noreply.github.com>
2024-11-06 17:57:28 +0530

    Add files via upload

--- slack.py
+++ slack.py
@@ -9,7 +9,7 @@ from pathlib import Path
 # loading environment variable
 from dotenv import load_dotenv
 
-from flask import Flask
+from flask import Flask, request, Response
 
 from slackeventsapi import SlackEventAdapter
 
@@ -27,21 +27,36 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGING_SECRET'],'/slack/even
 client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")["user_id"]
 
+message_counts = {}
 # sending message using slack client
 
 @slack_event_adapter.on('message')
+
 
 def message(payload):
     event = payload.get('event',{})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    print(text)
     
     if BOT_ID != user_id:
-        client.chat_postMessage(channel='#llm', text=input('Input something'))
+        if user_id in message_counts:
+            message_counts[user_id] +=1
+        else:
+            message_counts[user_id] = 1
+        client.chat_postMessage(channel='#llm', text='OK')
     
-# here is new comment
+# handling slash command
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+    # print(data)
+    client.chat_postMessage(channel=channel_id  , text=f'Messages : {message_count}')
+    
+    return Response(), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
